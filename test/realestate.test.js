151acfa9f104dd661161a515b032d1f2e51c66a1
james wasonga <James-Wasonga>
2023-05-14 20:29:36 +0300

    updated test

--- test/realestate.test.js
+++ test/realestate.test.js
@@ -8,24 +8,24 @@ contract('RealEstate', (accounts) => {
   });
   
   
-  // it("should be able to buy a registered property", async () => {
-  //   await a.addProperty("Kisumu", "1000000000000000000", "image4", "Property 1");
-  //   const val = 10 ** 18
-  //   const b = await a.buyProperty(1, {from: accounts[0], value: val.toString()});
-  //   // console.log(b);
+  it("should be able to buy a registered property", async () => {
+    await a.addProperty("Kisumu", "1000000000000000000", "image4", "Property 1");
+    const val = 10 ** 18
+    const b = await a.buyProperty(1, {from: accounts[0], value: val.toString()});
+    // console.log(b);
 
-  //   let c = await a.properties(1);
-  //   console.log(c);
-  //   assert.equal(c.isForSale, false);
+    let c = await a.properties(1);
+    console.log(c);
+    assert.equal(c.isForSale, false);
     
-  // })
+  })
 
-  // it("should be able to display a registered property", async() => {
-  //   await a.addProperty("Kisumu", "1", "image4", "A3");
-  //   let c = await a.properties(1);
-  //   assert.equal(c.location, "Kisumu");
+  it("should be able to display a registered property", async() => {
+    await a.addProperty("Kisumu", "1", "image4", "A3");
+    let c = await a.properties(1);
+    assert.equal(c.location, "Kisumu");
 
-  // })
+  })
   it("should be able to display the properties of changeOwnership", async() => {
     await a.addProperty("Kisumu", "1000000000000000000", "image4", "A3", {from: accounts[0]});
     const val = 10 ** 18
@@ -37,11 +37,11 @@ contract('RealEstate', (accounts) => {
     console.log(c)
 
 
-    // assert.equal(c.isForSale, false);
-    // await a.changeOwnership(1, {from: accounts[1]})
-    // c = await a.properties(1);
-    // assert.equal(c.isForSale, true);
-    // console.log(c)
+    assert.equal(c.isForSale, false);
+    await a.changeOwnership(1, {from: accounts[1]})
+    c = await a.properties(1);
+    assert.equal(c.isForSale, true);
+    console.log(c)
   })
 
   
